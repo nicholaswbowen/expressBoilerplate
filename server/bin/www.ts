1b9cd847c65e665df0aba49ca7a97eb3a1399b16
Nicholas Bowen <nicholas.w.bowen@gmail.com>
2017-05-03 16:18:40 -0700

    created more of the scaffold, fleshed out www.ts

--- server/bin/www.ts
+++ server/bin/www.ts
@@ -1,8 +1,14 @@
 let app = require('../main');
-let debug = require('debug')('myapp:server');
 let http = require('http');
 let port = normalizePort(process.env.PORT || '3000');
+let debug = require('debug')('myapp:server');
+app.set('port',port);
 
+let server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening',onListening);
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -18,4 +24,37 @@ function normalizePort(val) {
   }
 
   return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+  console.log('Listening on ' + bind);
 }
--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
 import * as mongoose from "mongoose";
+//mongoose's promise is deprecated, therefore you set it to the global promise
 (<any> mongoose).Promise = global.Promise;
+
 require('dotenv').config();
 let app = express();
-console.log(process.env.MONGO_URI)
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('sup')
+    //run database dependent methods here.
   })
